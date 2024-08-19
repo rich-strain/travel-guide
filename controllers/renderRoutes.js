e2a4866d74f3fe030bf75ae4785724b72fd43342
Eduardo Leon <eduardojleonrt@gmail.com>
2024-08-19 19:18:52 -0400

    Add handlebars to the project

--- controllers/renderRoutes.js
+++ controllers/renderRoutes.js
@@ -9,15 +9,21 @@ const withAuth = require('../utils/auth');
 
 // GET all Destinations for homepage
 router.get('/', async (req, res) => {
-  console.log('Render Homepage From homeRoutes.js');
+  console.log('Render Homepage From renderRoutes.js');
   // findAll Destinations join with User data
   try {
-    const destinationData = await Destination.findAll({ include: [{ model: User }] });
+    const destinationData = await Destination.findAll({
+      include: [{ model: User }]
+    });
+
     // Serialize data so the template can read it
-    //const destinations = destinationData.map((destination) => destination.get({ plain: true }));
+    const destinations = destinationData.map((destination) => destination.get({ plain: true }));
+
     // Pass serialized data and session into template
-    //res.render('homepage', { destinations });
-    res.status(200).json(destinationData);
+    res.render('homepage', {
+      destinations,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -26,30 +32,51 @@ router.get('/', async (req, res) => {
 // GET Destination By id
 router.get('/destination/:id', withAuth, async (req, res) => {
   console.log(`Render Page By Destination ID: ${req.params.id}`);
-  //findByPK Destination join with User data
+  // findByPK Destination join with User data
   try {
-    const destinationData = await Destination.findByPk(req.params.id, { include: [{ model: User }] });
+    const destinationData = await Destination.findByPk(req.params.id, {
+      include: [{ model: User }]
+    });
+
     if (!destinationData) {
       res.status(404).json({ message: 'Destination Not Found!' });
       return;
     }
 
     // Serialize data so the template can read it
-    //const destination = destinationData.get({ plain: true });
-    // Pass serialized data and session into template
-    //res.render('destination', { destination });
+    const destination = destinationData.get({ plain: true });
 
-    res.status(200).json(destinationData);
+    // Pass serialized data and session into template
+    res.render('destination', {
+      destination,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     const errMessage = { message: err.name };
     res.status(500).json(errMessage);
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', async (req, res) => {
+// Use withAuth middleware to prevent access to the profile route
+router.get('/profile', withAuth, async (req, res) => {
   console.log('Render Profile Page');
-  res.send('Render Profile Page');
+  // findByPK User data with associated Destinations
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      include: [{ model: Destination }],
+    });
+
+    // Serialize user data
+    const user = userData.get({ plain: true });
+
+    // Pass user data and session into template
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
