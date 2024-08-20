71df50366048e130318539986bdc98118ceb1176
Rich <richardstrain@gmail.com>
2024-08-20 19:42:14 -0400

    Basic Version of File Uploader

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 // Importing the route endpoints
 const userRoutes = require('./userRoutes');
 const destinationRoutes = require('./destinationRoutes');
+const blogRoutes = require('./blogRoutes');
 
 // Middleware to use the route endpoints
 router.use('/users', userRoutes);
 router.use('/destinations', destinationRoutes);
+router.use('/blogs', blogRoutes);
 
 // Export the router
 module.exports = router;
