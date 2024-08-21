7b36bc04386ffde71c13be4ea46eb3131c525e1a
Rich <richardstrain@gmail.com>
2024-08-20 22:20:14 -0400

    Update Blog Route

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const renderRoutes = require('./renderRoutes');
+const blogRoutes = require('./api/blogRoutes');
 
 // Use the rendering routes
 router.use('/', renderRoutes);
 
 // Use the API routes
 router.use('/api', apiRoutes);
+
+// Use the blog routes
+router.use('/api/blogs', blogRoutes);
 
 // Export the router
 module.exports = router;
