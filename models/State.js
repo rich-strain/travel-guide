6329923968ec9fdbce68ef09d333d59e35794540
Rich <richardstrain@gmail.com>
2024-08-20 20:55:01 -0400

    State Model and Data

---
+++ models/State.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class State extends Model {}
+
+State.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    abbreviation: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'state',
+  }
+);
+module.exports = Image;
