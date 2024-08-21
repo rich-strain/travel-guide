ae1708fd6ef33aff38308136c2869be938fedb84
JordanYoungCodes <168236369+JordanYoungCodes@users.noreply.github.com>
2024-08-20 21:25:58 -0400

    small model fixes

--- models/Destination.js
+++ models/Destination.js
@@ -28,13 +28,7 @@ Destination.init(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
+
   },
   {
     sequelize,
