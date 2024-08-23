1e4274dde51b1e88b5e7c5494d7a9c428f4d3c9f
Rich <richardstrain@gmail.com>
2024-08-23 06:52:16 -0400

    Possibly Correct DB Relationship

--- utils/byteScaleKey.js
+++ utils/byteScaleKey.js
@@ -1,5 +1,4 @@
-// Purpose: Middleware to check if the user is logged in before allowing them to access certain routes
-
+// Purpose: Middleware to pass Bytescale API key to the request object
 const setBytescaleAPI = (req, res, next) => {
   req.BYTESCALE_API_KEY = process.env.BYTESCALE_API_KEY;
   next();
